992614c6b1d16d94dbf1947975df42ee7f08fa62
Yash Bonde <bonde.yash97@gmail.com>
2019-05-07 13:05:17 +0530

    Make Release version

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
-from setuptools import setup
+import setuptools
 
-'''
 with open("README.md", "r") as fh:
     long_description = fh.read()
-'''
-long_description = 'Software to Handle AI Research Needs'
 
-setup(name='aistudio',
-      version='0.1',
+setuptools.setup(name='aistudio',
+      version='0.0.1',
       author="Yash Bonde",
       author_email="bonde.yash97@gmail.com",
       description="A Simple program to Handle AI Research Needs",
       long_description=long_description,
-      long_description_content_type="text",
+      long_description_content_type="text/markdown",
       url="https://github.com/yashbonde/aistudio",
       packages=setuptools.find_packages(),
       classifiers=[
